044afcfa67c052c70e0f8684626e6ecc814b8ac8
Alexandros Stavrou <alexandros.stavrou@camelotls.com>
2022-05-10 01:34:19 +0300

    [master] Setup Unit Testing - Vitest, Jest, happy-dom, types-jest, vue-test-utils, vue3-jest, c8, ts-jest

---
+++ test/NamesList.test.ts
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import NameList from '../src/components/NamesList.vue'
+import { it, describe, expect, test } from 'vitest';
+
+
+test('mount component', async () => {
+    expect(NameList).toBeTruthy();
+
+})
