75c4c5f168b553302ee6e890aaf5106227844390
Alexandros Stavrou <alexandros.stavrou@camelotls.com>
2022-05-12 02:22:44 +0300

    [master] Add Styles to input buttons and some basic staff

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,14 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import {test} from './utils/helpers'
 import HelloWorld from './components/HelloWorld.vue'
-import NamesList from './components/NamesList.vue'
+import NamesList from './components/namesList/NamesList.vue'
 
 </script>
 
 <template>
   <NamesList></NamesList>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/namesList/NamesList.vue
@@ -0,0 +1,49 @@
+<template>
+  <main class="main-container">
+    <section class="main-container__col">
+      <ol id="listOfNames">
+        <li v-for="name in state.names">{{name}}</li>
+
+      </ol>
+    </section>
+    <section class="main-container__col">
+      <div class="form-input__container">
+        <input id="name" type="text" v-model="state.nameToAdd" class="form-input__text" placeholder="Enter a name">
+        <label for="name" class="form-input__label">Enter a name</label>
+        <span class="form-input__decoration-line"></span>
+        <button id="addNameBtn" class="form-input__button--add" type="button" @click="addName(state.nameToAdd)">+</button>
+      </div>
+      <div>
+        <button id="runTheWheel" class="form-input__button--run" type="button" @click="runTheWheel">Run The Wheel</button>
+        <h2 id="winner">{{state.winner}}</h2>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { test } from "../../utils/helpers";
+import { reactive } from 'vue';
+
+const state = reactive({
+  names : [],
+  winner : '',
+  nameToAdd: ''
+});
+
+function addName(arg) {
+  state.names.push(arg);
+  test('test', state.names);
+  state.nameToAdd = '';
+  test('test2', state.nameToAdd);
+}
+
+function runTheWheel() {
+  const index = Math.floor(Math.random() * state.names.length)
+
+  state.winner = state.names[index];
+}
+
+</script>
+
+<style src="./_namesList.scss"></style>
