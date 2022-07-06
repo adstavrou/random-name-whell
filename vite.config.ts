6931f963b2233fb0fb3e3976edd669cd6f7f73b4
Alexandros Stavrou <alexandros.stavrou@camelotls.com>
2022-07-06 17:21:42 +0300

    [master] add I18n support

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {vueI18n} from "@intlify/vite-plugin-vue-i18n";
 import path from 'path';
 
 const pathSrc = path.resolve(__dirname, './src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      vueI18n({
+        defaultSFCLang: 'json5',
+        include: `${pathSrc}/assets/i18n/**`,
+      })
+  ],
   css: {
     preprocessorOptions: {
       scss: {
